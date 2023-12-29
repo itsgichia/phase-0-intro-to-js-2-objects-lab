905eddea15c49449eeb1a0ceb3ac1b6c79ea9343
itsgichia <gichiamuiruri@gmail.com>
2023-12-29 17:15:59 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+
+
+let employee={
+    name:"Gichia",
+    streetAdress:"Roysambu"
+};
+function updateEmployeeWithKeyAndValue(object,key,value){
+    let newObj={...object}
+    newObj[key]=value;
+    return newObj;
+
+}
+updateEmployeeWithKeyAndValue(employee,"sam","11 Broadway");
+
+function destructivelyUpdateEmployeeWithKeyAndValue(object,key,value){
+    object[key]=value;
+    return object;
+
+}
+function deleteFromEmployeeByKey(object,key){
+    let newObj={...object}
+    delete newObj[key]
+    return newObj;
+}
+deleteFromEmployeeByKey(employee,streetAdress);
+
+function destructivelyDeleteFromEmployeeByKey(object,key){
+    delete object[key];
+    return object;
+}
